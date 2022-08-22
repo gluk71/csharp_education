c0409681f94eb5fdef2d74f6752af6cdf53e3777
gluk71 <111636696+gluk71@users.noreply.github.com>
2022-08-22 20:53:01 +0300

    Zadacha 2
    Решение

---
+++ Program2.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+var n1 = 5;
+var n2 = 7;
+if (n1 > n2)
+    Console.WriteLine(n1);
+else 
+    Console.WriteLine(n2);
+var n11 = 2;
+var n21 = 10;
+if (n11 > n21)
+    Console.WriteLine(n11);
+else 
+    Console.WriteLine(n21);
+var n12 = -9;
+var n22 = -3;
+if (n12 > n22)
+    Console.WriteLine(n12);
+else 
+    Console.WriteLine(n22);

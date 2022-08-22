642c071a819d5da04bf2166ef69ca1e5244399ed
gluk71 <111636696+gluk71@users.noreply.github.com>
2022-08-22 20:53:48 +0300

    Zadacha4

---
+++ Program4.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+            int num_1 = 2;
+            int num_2 = 3;
+            int num_3 = 7;
+
+            int max = num_1;
+
+            if (num_2 > max)
+            {
+                max = num_2;
+            }
+
+            if (num_3 > max)
+            {
+                max = num_3;
+            }
+Console.WriteLine("Наибольшее из введённых чисел 1 -> " + max);
+            int num_11 = 44;
+            int num_21 = 5;
+            int num_31 = 78;
+
+            int max1 = num_11;
+
+            if (num_21 > max1)
+            {
+                max1 = num_21;
+            }
+
+            if (num_31 > max1)
+            {
+                max1 = num_31;
+            }
+Console.WriteLine("Наибольшее из введённых чисел 2 -> " + max1);
+            int num_12 = 22;
+            int num_22 = 3;
+            int num_32 = 9;
+
+            int max2 = num_12;
+
+            if (num_22 > max2)
+            {
+                max2 = num_22;
+            }
+
+            if (num_32 > max2)
+            {
+                max2 = num_32;
+            }
+Console.WriteLine("Наибольшее из введённых чисел 3 -> " + max2);

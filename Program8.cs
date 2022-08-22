64dc774eabeff2466325e5a4c4c29f62275d0fd0
gluk71 <111636696+gluk71@users.noreply.github.com>
2022-08-22 20:55:16 +0300

    Zadacha8

---
+++ Program8.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int k = 5;
+for (int i = 1; i <= k; i++)
+    if (i % 2 == 0)
+        Console.Write(i + ",");
+
+int l = 8;
+for (int i = 1; i <= l; i++)
+    if (i % 2 == 0)
+    Console.Write(i + ",");

5a1206129e433dbc78905c290d0238612d650c31
gluk71 <111636696+gluk71@users.noreply.github.com>
2022-08-22 20:54:37 +0300

    Zadacha 6

---
+++ Program6.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n=4;
+Console.WriteLine(n % 2 == 0 ?  "Число " +  n  + " четное " : "Число " +  n  + " нечетное ");
+int n1=-3;
+Console.WriteLine(n1 % 2 == 0 ?  "Число " +  n1  + " четное " : "Число " +  n1  + " нечетное ");
+int n2=7;
+Console.WriteLine(n2 % 2 == 0 ?  "Число " +  n2  + " четное " : "Число " +  n2  + " нечетное ");

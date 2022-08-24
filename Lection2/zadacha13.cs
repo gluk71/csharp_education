a353f4d7dbcef93a7634594cdc264861a5cf8ebe
gluk71 <111636696+gluk71@users.noreply.github.com>
2022-08-24 18:06:13 +0300

    Create zadacha13.cs

---
+++ Lection2/zadacha13.cs
@@ -0,0 +1,20 @@
+void Out3Digit(char[] theArray)
+        {
+            Console.Write("Выводим третий символ - ");
+            Console.WriteLine(theArray[2]);
+        }
+
+        Console.WriteLine("Enter number.....");      //вводим строку а не число
+        var number = Console.ReadLine();
+        if(number is null){
+            return;
+        }
+
+        if(number.Length < 3){
+            Console.WriteLine("Число очень маленькое!");
+            return;
+        }
+
+        char[] theArray = number.ToCharArray();      //конвертируем число в массив 
+
+        Out3Digit(theArray);

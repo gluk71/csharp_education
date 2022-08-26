f11a7055a20c19aa437d95d3b06dffc4fa7a5630
gluk71 <111636696+gluk71@users.noreply.github.com>
2022-08-26 13:26:37 +0300

    Create zadacha19.cs

---
+++ Seminar/Lesson_3/zadacha19.cs
@@ -0,0 +1,36 @@
+bool IsPalindrome(int number)
+            {
+                if (number < 0)
+                {
+                    throw new ArgumentException($"{number} is less than zero!");
+                }
+                int numberTemp = number;
+                int value;
+
+                int numberRevers = 0;
+
+                while (numberTemp > 0)
+                {
+                    value = numberTemp % 10;           
+                    numberRevers = numberRevers * 10 + value;
+
+                    numberTemp /= 10;
+                }
+
+                if (numberRevers == number)
+                {
+                    Console.WriteLine("True");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("False");
+                    return false;
+                }
+
+            }
+
+            Console.WriteLine("Enter number.....");      //вводим строку а не число
+            int a = Convert.ToInt32(Console.ReadLine());
+            IsPalindrome(a);
+           

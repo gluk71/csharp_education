cb947748e53a631bc0e332b8661f49264a44b22c
gluk71 <111636696+gluk71@users.noreply.github.com>
2022-08-29 22:53:48 +0300

    Update Zadacha 27.cs
    добавил второе решение

--- Seminar 4/Zadacha 27.cs
+++ Seminar 4/Zadacha 27.cs
@@ -1,3 +1,4 @@
+//самое простое решение что можно предложить 
 Console.WriteLine("Enter number:");
 int a = int.Parse(Console.ReadLine());
             int s = 0;
@@ -7,4 +8,23 @@ int a = int.Parse(Console.ReadLine());
                 a = a / 10;
 
             }
- Console.WriteLine(s);
+ Console.WriteLine(s);  //
+
+//решение можно  переписать и так
+int s = 0;
+
+int outSumma(int num)
+{
+    while (num > 0)
+       {
+        s = s + num % 10;
+        num = num / 10;
+       }
+     return s;
+ }
+
+  Console.WriteLine("Enter number:");
+  int numer = int.Parse(Console.ReadLine());
+  outSumma(numer);
+
+  Console.WriteLine(s);

d7fb9f78ac11fdf6203c771ef3fc98f316c46359
gluk71 <111636696+gluk71@users.noreply.github.com>
2022-09-18 02:23:43 +0300

    Create zadacha 64.cs

---
+++ Seminar 9/zadacha 64.cs
@@ -0,0 +1,16 @@
+void showNumm(int m, int n)
+{
+    if (n > m)
+    {
+        showNumm(m, n - 1);
+    }
+    Console.Write(n + "\t");
+}
+
+Console.Write("Input initial value: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input final value: ");
+int b = Convert.ToInt32(Console.ReadLine());
+showNumm(a,b);
+Console.ReadLine();

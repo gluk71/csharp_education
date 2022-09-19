ff7e3aa6f5320bcb1be4183a36690dceadb2f1fe
gluk71 <111636696+gluk71@users.noreply.github.com>
2022-09-19 18:06:23 +0300

    Update zadacha 64.cs

--- Seminar 9/zadacha 64.cs
+++ Seminar 9/zadacha 64.cs
@@ -1,16 +1,25 @@
-void showNumm(int m, int n)
-{
-    if (n > m)
-    {
-        showNumm(m, n - 1);
-    }
-    Console.Write(n + "\t");
-}
+ //Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+        static void Main(string[] args)
+        {
+            void showNumm(int m, int n)
+            {
+                if (n > m)
+                {
+                    showNumm(m, n - 1);
+                }
 
-Console.Write("Input initial value: ");
-int a = Convert.ToInt32(Console.ReadLine());
+                if (m > n)
+                {
+                    showNumm(n,m);
+                }
 
-Console.Write("Input final value: ");
-int b = Convert.ToInt32(Console.ReadLine());
-showNumm(a,b);
-Console.ReadLine();
+                Console.Write(n + "\t");
+            }
+
+            Console.Write("Input initial value: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input final value: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            showNumm(a, b);
+            Console.ReadLine();
